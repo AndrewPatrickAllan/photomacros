d06e186619c824418d241c3011490722b92e7307
Osmar234 <66699163+Osmar234@users.noreply.github.com>
2024-10-28 19:39:23 +0100

    split from processed to train 70%, val 15% and test 15%

--- photomacros/photomacros/features.py
+++ photomacros/photomacros/features.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import typer
 from loguru import logger
 from tqdm import tqdm
-
-from photomacros.config import PROCESSED_DATA_DIR
+from torch.utils.data import random_split, DataLoader
+from config import PROCESSED_DATA_DIR
 
 app = typer.Typer()
 
@@ -12,15 +12,13 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
+    input_path: Path = PROCESSED_DATA_DIR /,
+    output_path: Path = PROCESSED_DATA_DIR /,
     # -----------------------------------------
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+
     logger.success("Features generation complete.")
     # -----------------------------------------
 
