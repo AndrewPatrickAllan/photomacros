b0f14d5ce0458bb571ce6de8c1e878ddce304852
Andrew11232 <andrewallan9797@gmail.com>
2025-01-13 19:08:55 +0100

    adding osmars changes to train, predict eval

--- modeling/evaluate.py
+++ modeling/evaluate.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from photomacros.config import PROCESSED_DATA_DIR, REPORTS_DIR
 
 
-# we have created evaluate.py to evaluate the model predictions generated in predict.py 
+# we have created evaluate.py to evaluate the model predictions generated in predict.py
 
 def load_predictions(predictions_path):
     """
@@ -26,7 +26,7 @@ def evaluate_predictions(predictions, ground_truth_labels):
     """
     # Extract predicted and actual labels
     y_pred = predictions  # Assuming predictions are saved as class indices
-    y_true = ground_truth_labels["label"].values
+    y_true = ground_truth_labels["ground_truth_label"].values
 
     # Calculate metrics
     accuracy = accuracy_score(y_true, y_pred)
@@ -37,7 +37,7 @@ def evaluate_predictions(predictions, ground_truth_labels):
 def main(
     
     predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.pt",  # from predict.py
-    test_labels_path: Path = PROCESSED_DATA_DIR / "test_labels.pt",  # from predict.py
+    test_labels_path: Path = PROCESSED_DATA_DIR / "test_labels.csv",  # from predict.py
     metrics_output_path: Path = REPORTS_DIR / "metrics.json"
 ):
     """
